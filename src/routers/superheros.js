b799ae38b77215f926c76ab9613bfca0176cf053
YakymenkoOleksandr <93155369+YakymenkoOleksandr@users.noreply.github.com>
2024-10-31 21:20:39 +0200

    addaddcomit2
    Додавання фото, валідація.

--- src/routers/superheros.js
+++ src/routers/superheros.js
@@ -1,5 +1,8 @@
 import { Router } from 'express';
 
+import { validateBody } from '../middlewares/validateBody.js';
+import { createSuperheroSchema } from '../validation/superheros.js';
+
 import {
   getSuperherosController,
   getSuperheroByIdController,
@@ -10,22 +13,40 @@ import {
 } from '../controllers/superheros.js';
 import { ctrlWrapper } from '../utils/ctrlWrapper.js';
 import { upload } from '../middlewares/multer.js';
+import { isValidId } from '../middlewares/isValidId.js';
 
 const router = Router();
 
 router.get('/superheros', ctrlWrapper(getSuperherosController));
 
-router.get('/superheros/:superheroId', ctrlWrapper(getSuperheroByIdController));
+router.get(
+  '/superheros/:superheroId',
+  ctrlWrapper(getSuperheroByIdController),
+);
 
-router.post('/superheros', upload.single('images'), ctrlWrapper(createSuperheroController));
+router.post(
+  '/superheros',
+  validateBody(createSuperheroSchema),
+  upload.single('images'),
+  ctrlWrapper(createSuperheroController),
+);
 
 router.delete(
   '/superheros/:superheroId',
   ctrlWrapper(deleteSuperheroController),
 );
 
-router.put('/superheros/:superheroId', upload.single('images'), ctrlWrapper(upsertSuperheroController));
+router.put(
+  '/superheros/:superheroId',
+  validateBody(createSuperheroSchema),
+  upload.single('images'),
+  ctrlWrapper(upsertSuperheroController),
+);
 
-router.patch('/superheros/:superheroId', upload.single('images'), ctrlWrapper(patchSuperheroController));
+router.patch(
+  '/superheros/:superheroId',
+  upload.single('images'),
+  ctrlWrapper(patchSuperheroController),
+);
 
 export default router;
---
+++ src/validation/superheros.js
@@ -0,0 +1,15 @@
+import Joi from 'joi';
+
+export const createSuperheroSchema = Joi.object({
+  nickname: Joi.string().min(3).max(30).required().messages({
+    'string.base': 'Nickname should be a string',
+    'string.min': 'Nickname should have at least 3 characters',
+    'string.max': 'Nickname should have at most 30 characters',
+    'any.required': 'Nickname is required',
+  }),
+  real_name: Joi.string().min(3).max(30).required(),
+  origin_description: Joi.string().min(3).max(1000).required(),
+  superpowers: Joi.array().items(Joi.string().min(3).max(100)).required(),
+  catch_phrase: Joi.string().min(3).max(50).required(),
+  images: Joi.array().items(Joi.string().uri()).required(),
+});
