8eedf73b9d9b1a301da59f9348300b476e2528e0
YakymenkoOleksandr <93155369+YakymenkoOleksandr@users.noreply.github.com>
2024-12-05 14:42:35 +0200

    addcomit
    Додано зміну паролю за допомогою електронної пошти.

--- src/constants/index.js
+++ src/constants/index.js
@@ -8,6 +8,14 @@ export const CLOUDINARY = {
   API_SECRET: 'API_SECRET',
 };
 
+export const SMTP = {
+  SMTP_HOST: 'SMTP_HOST',
+  SMTP_PORT: 'SMTP_PORT',
+  SMTP_USER: 'SMTP_USER',
+  SMTP_PASSWORD: 'SMTP_PASSWORD',
+  SMTP_FROM: 'SMTP_FROM',
+};
+
 export const SWAGGER_PATH = path.join(process.cwd(), 'docs', 'swagger.json');
 
 export const FIFTEEN_MINUTES = 15 * 60 * 1000;
--- src/services/auth.js
+++ src/services/auth.js
@@ -4,6 +4,10 @@ import { UsersCollection } from '../db/models/user.js';
 import { randomBytes } from 'crypto';
 import { FIFTEEN_MINUTES, ONE_DAY } from '../constants/index.js';
 import { SessionsCollection } from '../db/models/session.js';
+import jwt from 'jsonwebtoken';
+import { SMTP } from '../constants/index.js';
+import { env } from '../utils/env.js';
+import { sendEmail } from '../utils/sendMail.js';
 
 export const registerUser = async (payload) => {
   const user = await UsersCollection.findOne({ email: payload.email });
@@ -87,3 +91,26 @@ export const refreshUsersSession = async ({ sessionId, refreshToken }) => {
   });
 };
 
+export const requestResetToken = async (email) => {
+  const user = await UsersCollection.findOne({ email });
+  if (!user) {
+    throw createHttpError(404, 'User not found');
+  }
+  const resetToken = jwt.sign(
+    {
+      sub: user._id,
+      email,
+    },
+    env('JWT_SECRET'),
+    {
+      expiresIn: '15m',
+    },
+  );
+
+  await sendEmail({
+    from: env(SMTP.SMTP_FROM),
+    to: email,
+    subject: 'Reset your password',
+    html: `<p>Click <a href="${resetToken}">here</a> to reset your password!</p>`,
+  });
+};
