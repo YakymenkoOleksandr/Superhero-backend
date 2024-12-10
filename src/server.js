35c21f12887eeba0884a35e20fa7562059a6b24a
YakymenkoOleksandr <93155369+YakymenkoOleksandr@users.noreply.github.com>
2024-12-10 20:43:34 +0200

    addcomit
    Додано маршрут з локального серверу.

--- src/server.js
+++ src/server.js
@@ -15,19 +15,27 @@ const PORT = Number(env('PORT', '3000'));
 
 export const startServer = () => {
   const app = express();
-  const allowedOrigins = ['https://superhero-frontend-nine.vercel.app'];
+  const allowedOrigins = [
+    'https://superhero-frontend-nine.vercel.app',
+    'http://localhost:5173',
+  ];
 
   app.use(express.json());
-  app.use(cors({
-    origin: (origin, callback) => {
-      if (allowedOrigins.includes(origin)) {
-        callback(null, true);
-      } else {
-        callback(new Error('Not allowed by CORS'));
-      }
-    },
-    credentials: true, // Дозволяє передачу cookies
-  }));
+
+  app.use(
+    cors({
+      origin: (origin, callback) => {
+        if (!origin || allowedOrigins.includes(origin)) {
+          // Дозволяємо запити без заголовка Origin для наприклад Postman
+          callback(null, true);
+        } else {
+          callback(new Error('Not allowed by CORS'));
+        }
+      },
+      credentials: true, // Дозволяє передачу cookies
+    }),
+  );
+
   app.use(cookieParser());
 
   app.use(
