f9b2013217970b70d415487f92ce065deff82f46
YakymenkoOleksandr <93155369+YakymenkoOleksandr@users.noreply.github.com>
2024-12-11 22:38:48 +0200

    addcomit
    Прибрано алерти. Змінено та уніфіковано з бекендом валідацію.

--- src/validation/auth.js
+++ src/validation/auth.js
@@ -3,12 +3,30 @@ import Joi from 'joi';
 export const registerUserSchema = Joi.object({
   name: Joi.string().min(3).max(30).required(),
   email: Joi.string().email().required(),
-  password: Joi.string().required(),
+  password: Joi.string()
+  .min(8)
+  .pattern(new RegExp(".*[a-z].*"), "lowercase")
+  .pattern(new RegExp(".*[A-Z].*"), "uppercase")
+  .pattern(new RegExp(".*\\d.*"), "number")
+  .pattern(
+    new RegExp('[!@#$%^&*(),.?":{}|<>]'),
+    "special character"
+  )
+  .required(),
 });
 
 export const loginUserSchema = Joi.object({
   email: Joi.string().email().required(),
-  password: Joi.string().required(),
+  password: Joi.string()
+    .min(8)
+    .pattern(new RegExp(".*[a-z].*"), "lowercase")
+    .pattern(new RegExp(".*[A-Z].*"), "uppercase")
+    .pattern(new RegExp(".*\\d.*"), "number")
+    .pattern(
+      new RegExp('[!@#$%^&*(),.?":{}|<>]'),
+      "special character"
+    )
+    .required(),
 });
 
 export const requestResetEmailSchema = Joi.object({
