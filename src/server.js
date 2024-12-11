65fbe91be0ea1c5be1bc2bba0c0333a780fecc32
YakymenkoOleksandr <93155369+YakymenkoOleksandr@users.noreply.github.com>
2024-12-11 14:43:46 +0200

    addcomit
    Чомусь не відправляються крос запити. Спроба виправити.

--- src/server.js
+++ src/server.js
@@ -15,19 +15,22 @@ const PORT = Number(env('PORT', '3000'));
 
 export const startServer = () => {
   const app = express();
-  const allowedOrigins = ['https://superhero-frontend-nine.vercel.app'];
 
   app.use(express.json());
-  app.use(cors({
-    origin: (origin, callback) => {
-      if (allowedOrigins.includes(origin)) {
-        callback(null, true);
-      } else {
-        callback(new Error('Not allowed by CORS'));
-      }
-    },
-    credentials: true, // Дозволяє передачу cookies
-  }));
+
+ app.use(cors({
+  origin: (origin, callback) => {
+    const allowedOrigins = ['http://localhost:5173', 'https://superhero-frontend-nine.vercel.app'];
+    if (allowedOrigins.includes(origin) || !origin) {
+      callback(null, true);
+    } else {
+      callback(new Error('Not allowed by CORS'));
+    }
+  },
+  credentials: true, // Дозволити облікові дані (cookies)
+}));
+
+
   app.use(cookieParser());
 
   app.use(
